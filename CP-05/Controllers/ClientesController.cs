bf421a299eb63af415d690556ff5785fd92c05fc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-27 20:38:34 -0300

    feat: alterando arquivo ClientesController

--- CP-05/Controllers/ClientesController.cs
+++ CP-05/Controllers/ClientesController.cs
@@ -4,9 +4,41 @@ namespace CP_05.Controllers
 {
     public class ClientesController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+
+        public IActionResult Cadastro()
+
         {
+
             return View();
+
         }
+
+        [HttpPost]
+
+        public IActionResult Cadastro(ClienteViewModel cliente, string confirmacaoSenha)
+
+        {
+
+            if (cliente.Senha != confirmacaoSenha)
+
+            {
+
+                ModelState.AddModelError("Senha", "As senhas não coincidem.");
+
+                return View(cliente);
+
+            }
+
+            // Hash da senha antes de salvar (exemplo simples, use Identity para produção)
+
+            cliente.Senha = BCrypt.Net.BCrypt.HashPassword(cliente.Senha);
+
+            _clienteRepositorio.Cadastrar(cliente);
+
+            return RedirectToAction("Login");
+
+        }
+
     }
 }
