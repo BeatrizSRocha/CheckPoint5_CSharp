bc46118ecd3c9dbdf58ced5ec0d03d03c733a8fd
Beatriz Silva <126727432+BeatrizSRocha@users.noreply.github.com>
2026-04-27 20:36:18 -0300

    feat: criando arquivo Controller

---
+++ CP-05/Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP_05.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CP-05/obj/Debug/net8.0/CP-05.AssemblyInfo.cs
+++ CP-05/obj/Debug/net8.0/CP-05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CP-05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f021922a9ebeb1e4b76c40a3f3b281f3e9dfe70")]
 [assembly: System.Reflection.AssemblyProductAttribute("CP-05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CP-05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
